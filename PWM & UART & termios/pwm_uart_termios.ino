d11e2f5cb04a6f1505e2630b3bfc9ae43999e20b
Merve Müge Deliktaş <merve.muge.13@gmail.com>
2019-07-08 16:20:00 +0300

    Update pwm_uart_termios.ino

--- PWM & UART & termios/pwm_uart_termios.ino
+++ PWM & UART & termios/pwm_uart_termios.ino
@@ -17,39 +17,51 @@ void loop() {
     ch = Serial.readString();
     ch.trim();
     if (ch == "on" || ch == "ON") {
+      Serial.println("led is on");
       write(10);
     }
     else if (ch == "off" || ch == "OFF") {
+      Serial.println("led is off");
       write(0);
     }
     else if (ch == "1") {
+      Serial.println("brightness rate : %10");
       write(1);
     }
     else if (ch == "2") {
+      Serial.println("brightness rate : %20");
       write(2);
     }
     else if (ch == "3") {
+      Serial.println("brightness rate : %30");
       write(3);
     }
     else if (ch == "4") {
+      Serial.println("brightness rate : %40");
       write(4);
     }
     else if (ch == "5") {
+      Serial.println("brightness rate : %50");
       write(5);
     }
     else if (ch == "6") {
+      Serial.println("brightness rate : %60");
       write(6);
     }
     else if (ch == "7") {
+      Serial.println("brightness rate : %70");
       write(7);
     }    
     else if (ch == "8") {
+      Serial.println("brightness rate : %80");
       write(8);
     }
     else if (ch == "9") {
+      Serial.println("brightness rate : %90");
       write(9);
     }
     else if (ch == "10"){
+      Serial.println("led is on");
       write(10);
     }
     
