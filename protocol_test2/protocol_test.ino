d1f935e429aa79cb16a00dfbbdf4d08cfb87573d
Merve Müge Deliktaş <merve.muge.13@gmail.com>
2019-07-26 10:22:09 +0300

    ADD run_tg

--- protocol_test2/protocol_test.ino
+++ protocol_test2/protocol_test.ino
@@ -57,14 +57,45 @@ bool break_checker(int period ){
   
 }
 
-void flash(char * write_buffer) {
+int read_two_digit_data( char * write_buffer , int higher_order , int lower_order ){
+
+  int higher_digit = write_buffer[higher_order] - '0';
+  int lower_digit = write_buffer[lower_order] - '0';
+  
+  int value = ( higher_digit * 10 ) + lower_digit;
+  
+  return value;
+    
+}
+
+
+void run_tg( char * write_buffer ){
+  
   digitalWrite(led, LOW);
-  int check = 0;
+  
+  int period = read_two_digit_data( write_buffer, 6, 7);
+  int pwm = read_two_digit_data( write_buffer, 4, 5);
 
-  int sixth_index = write_buffer[6] - '0';
-  int seventh_index = write_buffer[7] - '0';
+  while (period > 0) {
 
-  int period = ( sixth_index * 10 ) + seventh_index;
+    if( !(break_checker(period)) ){
+      break;
+    }
+    
+    write(pwm);
+    if( !(break_checker(period)) ){
+      break;
+    }
+    
+    digitalWrite(led, LOW);
+  }
+  
+}
+
+void flash(char * write_buffer) {
+  
+  digitalWrite(led, LOW);
+  int period = read_two_digit_data( write_buffer, 6, 7);
 
   while (period > 0) {
 
@@ -95,11 +126,10 @@ void guidance_to_start_menu(char * write_buffer) {
     pwm(write_buffer);
   }
   else if (write_buffer[0] == '2') {
-    //Serial.println("flash");
     flash(write_buffer);
   }
   else if (write_buffer[0] == '3') {
-    Serial.println("run tg");
+    run_tg( write_buffer);
   }
   else if (write_buffer[0] == '4') {
     Serial.println("led switch");
