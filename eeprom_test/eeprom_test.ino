ba4b22be13beb0d8981f5b2666d92681aa470c2c
Merve Müge Deliktaş <merve.muge.13@gmail.com>
2019-07-31 17:40:27 +0300

    Update eeprom_test.ino

--- eeprom_test/eeprom_test.ino
+++ eeprom_test/eeprom_test.ino
@@ -5,6 +5,23 @@ int led = 9;
 int button = 2;
 
 int temp = 0;
+int button_high = HIGH;
+int button_low = LOW;
+
+int counter = 0;
+
+void check_in_setup(int temp) {
+
+  switch (temp) {
+    case 0:
+      digitalWrite(led, LOW);
+      break;
+    case 1:
+      counter = 1;
+      digitalWrite(led, HIGH);
+      break;
+  }
+}
 
 void setup() {
   Serial.begin(115200);
@@ -12,31 +29,45 @@ void setup() {
   pinMode(button, INPUT);
 
   temp = EEPROM.read(addr);
-  
-  switch (temp) {
-    case 0:
-      digitalWrite(led, LOW);
-      break;
-    case 1:
-      digitalWrite(led, HIGH);
-      break;
-  }
+  check_in_setup(temp);
+
+}
+
+void on() {
+  digitalWrite(led, HIGH);
+  EEPROM.write(addr, 1);
+}
+void off() {
+  digitalWrite(led, LOW);
+  EEPROM.write(addr, 0);
 }
 
 void loop() {
 
   while (1) {
     if (digitalRead(button) == HIGH) {
-      digitalWrite(led, HIGH);
-      EEPROM.write(addr, 1);
-      break;
+      Serial.println(counter);
+      if (counter == 2) {
+        counter = 0;
+      }
+
+      if (counter == 0 ) {
+        delay(1000);
+        counter++;
+        on();
+        break;
+      }
+      else if (counter == 1) {
+        delay(1000);
+        counter++;
+        off();
+        break;
+      }
     }
   }
 
   while (1) {
-    if (digitalRead(button) == HIGH) {
-      digitalWrite(led, LOW);
-      EEPROM.write(addr, 0);
+    if (digitalRead(button) == LOW) {
       break;
     }
   }
