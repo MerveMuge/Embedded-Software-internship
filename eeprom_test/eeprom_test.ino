3fa5fd83e1e41ec4400f13e2b4ad5637555c9481
Merve Müge Deliktaş <merve.muge.13@gmail.com>
2019-08-05 17:13:34 +0300

    Update eeprom_test.ino

--- eeprom_test/eeprom_test.ino
+++ eeprom_test/eeprom_test.ino
@@ -4,77 +4,6 @@ int addr = 0;
 int led = 9;
 int button = 2;
 
-void On() {
-  digitalWrite(led, HIGH);
-  EEPROM.write(addr, 0);
-}
-
-void Off() {
-  digitalWrite(led, LOW);
-  EEPROM.write(addr, 1);
-}
-
-bool is_button_not_high() {
-
-  if ( !(digitalRead(button) == HIGH) ) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-int Pwm() {
-
-  int brightness = 0;    // how bright the LED is
-  int fadeAmount = 5;    // how many points to fade the LED by
-  EEPROM.write(addr, 2);
-  delay(300);
-  while (1) {
-
-    if ( is_button_not_high() )
-      analogWrite(led, brightness);
-    else
-      break;
-
-    if ( is_button_not_high() )
-      brightness = brightness + fadeAmount;
-    else
-      break;
-
-    if ( is_button_not_high() ) {
-      if (brightness <= 0 || brightness >= 255) {
-        fadeAmount = -fadeAmount;
-      }
-    }
-    else {
-      break;
-    }
-
-    delay(30);
-
-  }
-}
-
-int Flash() {
-  delay(300);
-  EEPROM.write(addr, 3);
-  while (1) {
-
-    if ( is_button_not_high() )
-      digitalWrite(led, HIGH);
-    else
-      break;
-
-    delay(100);
-
-    if ( is_button_not_high() )
-      digitalWrite(led, LOW);
-    else
-      break;
-    delay(100);
-  }
-}
-
 enum opMode {
   ON,
   OFF,
@@ -108,27 +37,120 @@ struct opManager {
 
 } opManagerSt;
 
-int select_menu_item(int next_item) {
+void On() {
+  Serial.println("HIGH");
+  digitalWrite(led, HIGH);
+  EEPROM.write(addr, 0);
+}
+
+void Off() {
+  Serial.println("LOW");
+  digitalWrite(led, LOW);
+  EEPROM.write(addr, 1);
+}
+
+bool is_button_not_high() {
+
+  if ( !(digitalRead(button) == HIGH) ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+int Pwm() {
+  Serial.println("PWM");
+  int brightness = 0;    // how bright the LED is
+  int fade_amount = 5;    // how many points to fade the LED by
+  EEPROM.write(addr, 2);
+  delay(300);
+  while (1) {
+
+    if ( is_button_not_high() )
+      analogWrite(led, brightness);
+    else {
+      break;
+    }
+
+    if ( is_button_not_high() )
+      brightness = brightness + fade_amount;
+    else {
+      break;
+    }
+
+    if ( is_button_not_high() ) {
+      if (brightness <= 0 || brightness >= 255) {
+        fade_amount = -fade_amount;
+      }
+    }
+    else {
+      break;
+    }
+    delay(30);
+
+  }
+
+  return 1;
+}
+
+int Flash() {
+  delay(300);
+  Serial.println("FLASH");
+  EEPROM.write(addr, 3);
+  while (1) {
+
+    if ( is_button_not_high() ) {
+      digitalWrite(led, HIGH);
+    }
+    else {
+      break;
+    }
+    delay(100);
+
+    if ( is_button_not_high() ) {
+      digitalWrite(led, LOW);
+    }
+    else {
+      break;
+    }
+    delay(100);
+  }
+
+  return 1;
+}
+
+
+int Select_menu_item(int next_item) {
+
+  int result = 0;
 
   switch (next_item) {
     case 0:
-      Serial.println("HIGH");
-      On(); 
-      return 1;
+      On();
+      break;
     case 1:
-      Serial.println("LOW");
       Off();
-      return 1;
+      break;
     case 2:
-      Serial.println("PWM");
-      Pwm();
-      return 1;
+      if (Pwm() == 1) {
+        //Select_menu_item(3);
+        int next = opManagerSt.next();
+        Select_menu_item(next);
+        //result = 1;
+      }
+      break;
     case 3:
-      Serial.println("FLASH");
-      Flash();
-      return 1;
-
+      if ( Flash() == 1 ) {
+        //On();
+        //Select_menu_item(0);
+        //opManagerSt.next();
+        //result = 2;
+        int next = opManagerSt.next();
+        Select_menu_item(next);
+      }
+      break;
   }
+  return result;
 
 }
 
@@ -139,7 +161,7 @@ void setup() {
   pinMode(button, INPUT);
 
   int eeprom_read_value = EEPROM.read(addr);
-  select_menu_item( eeprom_read_value );
+  Select_menu_item( eeprom_read_value );
 
 }
 
@@ -150,9 +172,8 @@ void loop() {
 
       int next_item = opManagerSt.next();
 
-      if ( select_menu_item(next_item) == 1 ) {
-        break;
-      }
+      Select_menu_item(next_item);
+      break;
 
     }
   }
