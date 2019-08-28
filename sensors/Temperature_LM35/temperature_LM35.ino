231dddae6d501b0f4d209d62cb4fca489c8f9501
Merve Müge Deliktaş <merve.muge.13@gmail.com>
2019-08-28 15:49:25 +0300

    Update temperature_LM35.ino

--- sensors/Temperature_LM35/temperature_LM35.ino
+++ sensors/Temperature_LM35/temperature_LM35.ino
@@ -1,29 +1,15 @@
-int val;
-int tempPin = 1;
+#define TEMPERATURE_PIN A1
+
+#include "Temperature.h"
+
+Temperature temperature(TEMPERATURE_PIN);
 
 void setup()
 {
   Serial.begin(115200);
 }
+
 void loop()
 {
-  val = analogRead(tempPin);
-  float mv = ( val / 1024.0) * 5000;
-  float cel = mv / 10;
-  float farh = (cel * 9) / 5 + 32;
-
-  Serial.print("TEMPRATURE = ");
-  Serial.print(cel);
-  Serial.print("*C");
-  Serial.println();
-  delay(1000);
-
-  /* uncomment this to get temperature in farenhite
-    Serial.print("TEMPRATURE = ");
-    Serial.print(farh);
-    Serial.print("*F");
-    Serial.println();
-
-
-  */
+  temperature.evaluateTemperature();
 }
