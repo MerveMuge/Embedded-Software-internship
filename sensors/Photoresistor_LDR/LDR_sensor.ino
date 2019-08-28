2486d5052f6418acba181715570f1e884dd21e4d
Merve Müge Deliktaş <merve.muge.13@gmail.com>
2019-08-28 14:15:10 +0300

    ADD LDR library

--- sensors/Photoresistor_LDR/LDR_sensor.ino
+++ sensors/Photoresistor_LDR/LDR_sensor.ino
@@ -1,29 +1,17 @@
-#define ledPin 13
-#define ldrPin A0
+#define LED_PIN 13
+#define LDR_PIN A0
+
+#include "Photoresistor.h"
+
+Photoresistor photoresistor(LED_PIN, LDR_PIN);
 
 void setup() {
 
   Serial.begin(115200);
-  pinMode(ledPin, OUTPUT);
-  pinMode(ldrPin, INPUT);
 }
+
 
 void loop() {
 
-  int ldrStatus = analogRead(ldrPin);
-  
-  if (ldrStatus <= 200) {
-
-    digitalWrite(ledPin, HIGH);
-    Serial.print("Its DARK, Turn on the LED : ");
-    Serial.println(ldrStatus);
-
-  } else {
-
-    digitalWrite(ledPin, LOW);
-    Serial.print("Its BRIGHT, Turn off the LED : ");
-    Serial.println(ldrStatus);
-
-  }
-
+  photoresistor.ldr();
 }
